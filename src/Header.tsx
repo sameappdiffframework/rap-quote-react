50f3985dacd7e707e08a4be27c65fcc881423146
Colby M. White <colbywhite@users.noreply.github.com>
2021-09-03 17:20:21 -0500

    feat: add general css; add header

---
+++ src/Header.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import './Header.css';
+
+export default class Header extends React.Component {
+    render() {
+        return (
+            <header>
+                <nav>
+                    <ul>
+                        <li>
+                            {/*<a href="#" onClick="openModal()">Create quote</a>*/}
+                            <a href="#">Create quote</a>
+                        </li>
+                        <li>
+                            <a href="/">All quotes</a>
+                        </li>
+                        <li>
+                            <a href="/about.html">About</a>
+                        </li>
+                    </ul>
+                </nav>
+            </header>
+        );
+    }
+}
