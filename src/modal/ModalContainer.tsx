7792796765be6ddf225fda9173b16775582f1a67
Colby M. White <colbywhite@users.noreply.github.com>
2021-09-05 14:39:56 -0500

    refactor: use portal for modal

--- src/modal/ModalContainer.tsx
+++ src/modal/ModalContainer.tsx
@@ -1,10 +1,7 @@
+import ReactDOM from 'react-dom';
 import Overlay from './Overlay';
 
 export default function ModalContainer(props: { content: JSX.Element }) {
-    return (
-        <>
-            <Overlay/>
-            {props.content}
-        </>
-    );
+    const container = <> <Overlay/> {props.content} </>;
+    return ReactDOM.createPortal(container, document.body);
 }
